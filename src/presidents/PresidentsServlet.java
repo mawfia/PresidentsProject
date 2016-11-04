e955b2118ffcc5284d505200c41437dba7d0da00
Teratopia <kylewoodennis@gmail.com>
2016-11-04 17:28:17 -0600

    added pic urls to csv

--- src/presidents/PresidentsServlet.java
+++ src/presidents/PresidentsServlet.java
@@ -1,8 +1,10 @@
 package presidents;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -34,6 +36,12 @@ public class PresidentsServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+	}
+	
+	public void init(){
+		List<President> presidents = new ArrayList<President>();
+		
+		
 	}
 
 }
